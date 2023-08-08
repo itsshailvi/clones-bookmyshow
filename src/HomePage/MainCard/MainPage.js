c6ab5f6dc12dd95669cde8a260d0f3d2e03f131e
itsshailvi <itsshailvi@gmail.com>
2023-08-08 23:59:19 +0530

    search

--- src/HomePage/MainCard/MainPage.js
+++ src/HomePage/MainCard/MainPage.js
@@ -2,21 +2,31 @@ import { SettingsSystemDaydreamTwoTone } from '@mui/icons-material'
 import React, { useEffect, useState } from 'react'
 import AllCard from './AllCard'
 import '../../App.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { updateQuery, updateResults } from './../Header/searchSlice'
 
 const MainPage = () => {
   const [user, setUser] = useState([])
+  const dispatch = useDispatch();
+  const { query, results } = useSelector((state) => state.search);
 
   useEffect(() => {
     fetchMovie()
   }, [])
 
-  const fetchMovie = () => {
+  const fetchMovie = async () => {
     const header = {
       'Content-Type': 'application/json',
       'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkN2NkZDgyZTYzNTNkNTg3Mzc5YWMxZjdjNzc5N2JlYiIsInN1YiI6IjY0YTE0MjhiYzM5MGM1MDBlYjM1NTU0MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tw-vDY3w7kzHCUJHIVW9iKH-9RmwFhbUjMXcPvsWnPg'
     }
-    fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', { method: 'GET', headers: header })
-    .then(response => { return response.json() }).then(data => setUser(data.results))
+    if(!query){
+      fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', { method: 'GET', headers: header })
+      .then(response => { return response.json() }).then(data => setUser(data.results))
+    }else{
+      const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${bard}&include_adult=false&language=en-US&page=1`, { method: 'GET', headers: header })
+      .then(response => { return response.json() }).then(data => setUser(data.results))
+    }
+
   }
 
   console.log(user, 'test')
