0e48cead9352c1204e7d8033e5df719f8b72d31d
itsshailvi <itsshailvi@gmail.com>
2023-08-30 22:52:47 +0530

    react router basics

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import './App.css';
 import Login from './LoginPages/Login';
 import HomePage from './HomePage/HomePage';
 import {
-  createBrowserRouter,
-  RouterProvider,
+  BrowserRouter,
+  Route,
+  Routes,
 } from "react-router-dom";
+
+
 
 function App() {
   return (
     <div className="App">
       {/* <Login/> */}
-      <HomePage/>
+      {/* <HomePage/> */}
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/> 
+        <Route path="/login" element={<Login/>}/> 
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
